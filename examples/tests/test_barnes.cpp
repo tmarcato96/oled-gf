8e66c9f28b746b7e5cde595573a772f1258e5dc3
tmarcato <tmarcato@ethz.ch>
2025-04-03 12:18:36 +0200

    Modified CMakeLists.txt for compilation on MacOS with GCC-14

--- examples/tests/test_barnes.cpp
+++ examples/tests/test_barnes.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include <material.hpp>
 #include <basesolver.hpp>
+#include <material.hpp>
 #include <simulation.hpp>
 
 #include <Eigen/Core>
@@ -13,17 +13,17 @@ int main()
   const double wavelength = 530;
   std::vector<Layer> layers;
 
-  layers.emplace_back(Material("/src/mat/Al_Cent.csv", ','), -1.0);
+  layers.emplace_back(Material("../mat/Al_Cent.csv", ','), -1.0);
   layers.emplace_back(Material(1.9, 0.0), 50e-9);
-  layers.emplace_back(Material("/src/mat/CBP.csv", ','), 20e-9, true);
-  layers.emplace_back(Material("/src/mat/PEDOT_BaytronP_AL4083.csv", ','), 35e-9);
-  layers.emplace_back(Material("/src/mat/ITO.csv", ','), 150e-9);
+  layers.emplace_back(Material("../mat/CBP.csv", ','), 20e-9, true);
+  layers.emplace_back(Material("../mat/PEDOT_BaytronP_AL4083.csv", ','), 35e-9);
+  layers.emplace_back(Material("../mat/ITO.csv", ','), 150e-9);
   layers.emplace_back(Material(1.52, 0.0), 5000e-9);
   layers.emplace_back(Material(1.52, 0.0), -1.0);
 
   // Spectrum
   const double fwhm = 30;
-  GaussianSpectrum spectrum(450, 700, wavelength, fwhm/2.355);
+  GaussianSpectrum spectrum(450, 700, wavelength, fwhm / 2.355);
 
   // Create Solver
   auto simulation = std::make_unique<Simulation>(SimulationMode::ModeDissipation, layers, 10e-9, spectrum, 0.0, 2.0);
@@ -34,6 +34,8 @@ int main()
   Vector const& y = simulation->mFracPowerPerpUpPol.row(1).head(u.size());
   Vector const& yParapPol = simulation->mFracPowerParaUpPol.row(1).head(u.size());
   Vector const& yParasPol = simulation->mFracPowerParaUsPol.row(1).head(u.size());
+
+  std::cout << y.head(20) << '\n';
 
   // Plot
   matplot::semilogy(u, y)->line_width(2).color("red");
