c1d71fde96041d69614b3e06dd9b9f875ea7ef22
tmarcato <tmarcato@ethz.ch>
2025-04-04 15:32:55 +0200

    Final fix of simulation bug + public getter for mDipoleLayer in BaseSolver

--- examples/tests/test_barnes.cpp
+++ examples/tests/test_barnes.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include <material.hpp>
 #include <basesolver.hpp>
+#include <material.hpp>
 #include <simulation.hpp>
 
 #include <Eigen/Core>
@@ -13,31 +13,28 @@ int main()
   const double wavelength = 535;
   std::vector<Layer> layers;
 
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\Al_Cent.csv", ','), -1.0);
+  layers.emplace_back(Material("../mat/Al_Cent.csv", ','), -1.0);
   layers.emplace_back(Material(1.9, 0.0), 50e-9);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\CBP.csv", ','), 20e-9, true);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\PEDOT_BaytronP_AL4083.csv", ','), 35e-9);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\ITO.csv", ','), 150e-9);
+  layers.emplace_back(Material("../mat/CBP.csv", ','), 20e-9, true);
+  layers.emplace_back(Material("../mat/PEDOT_BaytronP_AL4083.csv", ','), 35e-9);
+  layers.emplace_back(Material("../mat/ITO.csv", ','), 150e-9);
   layers.emplace_back(Material(1.52, 0.0), 5000e-9);
   layers.emplace_back(Material(1.52, 0.0), -1.0);
 
   // Spectrum
   const double fwhm = 30;
-  GaussianSpectrum spectrum(450, 700, wavelength, fwhm/2.355);
+  GaussianSpectrum spectrum(450, 700, wavelength, fwhm / 2.355);
 
   // Create Solver
-  auto simulation = std::make_unique<Simulation>(SimulationMode::ModeDissipation, layers, 10e-9, wavelength, 0.0, 2.0);
+  auto simulation = std::make_unique<Simulation>(SimulationMode::ModeDissipation, layers, 10e-9, spectrum, 0.0, 2.0);
+  auto dipoleIndex = simulation->getDipoleIndex();
   simulation->run();
 
   // Mode dissipation figure
   Vector const& u = simulation->getInPlaneWavevector();
-  Vector const& y = simulation->mFracPowerPerpUpPol.row(1).head(u.size());
-  Vector const& yParapPol = simulation->mFracPowerParaUpPol.row(1).head(u.size());
-  Vector const& yParasPol = simulation->mFracPowerParaUsPol.row(1).head(u.size());
-
-  std::cout << y.size() << ' ' << y.head(10) << ' ' << y.tail(1) << std::endl;
-  std::cout << yParapPol.size() << ' ' << yParapPol.head(1) << ' ' << yParapPol.tail(1) << std::endl;
-  std::cout << yParasPol.size() << ' ' << yParasPol.head(1) << ' ' << yParasPol.tail(1) << std::endl;
+  Vector const& y = simulation->mFracPowerPerpUpPol.row(dipoleIndex - 1).head(u.size());
+  Vector const& yParapPol = simulation->mFracPowerParaUpPol.row(dipoleIndex - 1).head(u.size());
+  Vector const& yParasPol = simulation->mFracPowerParaUsPol.row(dipoleIndex - 1).head(u.size());
 
   // Plot
   matplot::semilogy(u, y)->line_width(2).color("red");
--- examples/tests/test_test.cpp
+++ examples/tests/test_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include <material.hpp>
 #include <basesolver.hpp>
+#include <material.hpp>
 #include <simulation.hpp>
 
 #include <Eigen/Core>
@@ -12,34 +12,34 @@ int main()
   // Set up stack
   std::vector<Layer> layers;
 
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\air.csv", ','), -1.0);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\ag.csv", ','), 200e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\mg_palik.csv", ','), 1000e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\tpd.csv", ','), 500e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\alq3_literature2.csv", ','), 200e-10, true);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\tpd.csv", ','), 500e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\pedot.csv", ','), 300e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\test_ito.csv", ','), 1600e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\glass_no_loss.csv", ','), 5000e-10);
-  layers.emplace_back(Material("C:\\Users\\mnouman\\oled-gf\\mat\\glass_no_loss.csv", ','), -1.0);
+  layers.emplace_back(Material("../mat/air.csv", ','), -1.0);
+  layers.emplace_back(Material("../mat/ag.csv", ','), 200e-10);
+  layers.emplace_back(Material("../mat/mg_palik.csv", ','), 1000e-10);
+  layers.emplace_back(Material("../mat/tpd.csv", ','), 500e-10);
+  layers.emplace_back(Material("../mat/alq3_literature2.csv", ','), 200e-10, true);
+  layers.emplace_back(Material("../mat/tpd.csv", ','), 500e-10);
+  layers.emplace_back(Material("../mat/pedot.csv", ','), 300e-10);
+  layers.emplace_back(Material("../mat/test_ito.csv", ','), 1600e-10);
+  layers.emplace_back(Material("../mat/glass_no_loss.csv", ','), 5000e-10);
+  layers.emplace_back(Material("../mat/glass_no_loss.csv", ','), -1.0);
 
   // Spectrum
-  //THINGS TO DO: MOVE SIMULATION CONSTRUCTORS, CREATE NEW UNIFORM SPECTRUM MODE
+  // THINGS TO DO: MOVE SIMULATION CONSTRUCTORS, CREATE NEW UNIFORM SPECTRUM MODE
   const double wavelength = 535;
-  //DipoleDistribution dipolePositions{};
-  //dipolePositions.dipolePositions = Vector::LinSpaced(11, 0, 20e-9);
+  // DipoleDistribution dipolePositions{};
+  // dipolePositions.dipolePositions = Vector::LinSpaced(11, 0, 20e-9);
 
   // Create Solver
-  auto simulation = std::make_unique<Simulation>(SimulationMode::ModeDissipation, layers, 0.0, wavelength, 0.0, std::cos(std::complex<double>(0.0, 1.8)).real());
+  auto simulation = std::make_unique<Simulation>(
+    SimulationMode::ModeDissipation, layers, 0.0, wavelength, 0.0, std::cos(std::complex<double>(0.0, 1.8)).real());
   simulation->run();
+  auto dipoleIndex = simulation->getDipoleIndex();
 
   // Mode dissipation figure
   Vector const& u = simulation->getInPlaneWavevector();
-  Vector const& y = simulation->mFracPowerPerpUpPol.row(1).head(u.size());
-  Vector const& yParapPol = simulation->mFracPowerParaUpPol.row(1).head(u.size());
-  Vector const& yParasPol = simulation->mFracPowerParaUsPol.row(1).head(u.size());
-  std::cout << yParapPol.size() << ' ' << yParapPol.head(5) << ' ' << yParapPol.tail(5) << std::endl;
-  std::cout << yParasPol.size() << ' ' << yParasPol.head(5) << ' ' << yParasPol.tail(5) << std::endl;
+  Vector const& y = simulation->mFracPowerPerpUpPol.row(dipoleIndex - 1).head(u.size());
+  Vector const& yParapPol = simulation->mFracPowerParaUpPol.row(dipoleIndex - 1).head(u.size());
+  Vector const& yParasPol = simulation->mFracPowerParaUsPol.row(dipoleIndex - 1).head(u.size());
 
   // Plot
   matplot::semilogy(u, y)->line_width(2).color("red");
