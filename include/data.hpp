2449f2fe9236e3692b4e0fcf0cac3c2b09b88867
moleculebits <lepton42@protonmail.ch>
2025-04-09 15:20:12 +0200

    data is now a namespace, not a class, and  might be migrated to material later

--- include/data.hpp
+++ include/data.hpp
@@ -4,7 +4,7 @@
 
 #include <forwardDecl.hpp>
 
-class Data {
-    public:
-        static Matrix loadFromFile(const std::string& filepath, size_t ncols, char delimiter='\t'); 
+namespace Data {
+
+        Matrix loadFromFile(const std::string& filepath, size_t ncols, char delimiter='\t');
 };
