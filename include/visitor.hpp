02531a6753bb4f7a7df5ebce5a12753306b303dd
moleculebits <lepton42@protonmail.ch>
2025-04-30 18:36:35 +0200

    expanded visitor (still very much a WIP)

--- include/visitor.hpp
+++ include/visitor.hpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <map>
+#include <stack>
 #include <matlayer.hpp>
 #include <jsonsimplecpp/node.hpp>
 #include <jsonsimplecpp/parser.hpp>
 
-template<class VisitorPolicy> using JsonObject = std::map<std::string, std::unique_ptr<Json::JsonNode<VisitorPolicy>>>;
-
-template<class VisitorPolicy> using JsonList = std::vector<std::unique_ptr<Json::JsonNode<VisitorPolicy>>>;
-
 struct ConfigVisitor {
-  void operator()(const std::unique_ptr<JsonObject<ConfigVisitor>>&);
-  void operator()(const std::unique_ptr<JsonList<ConfigVisitor>>&);
+
+  using JsonObject = std::map<std::string, std::unique_ptr<Json::JsonNode<ConfigVisitor>>>;
+  using JsonList = std::vector<std::unique_ptr<Json::JsonNode<ConfigVisitor>>>;
+  using ValueType = std::variant<std::string, double>;
+
+  void operator()(const std::unique_ptr<JsonObject>&);
+  void operator()(const std::unique_ptr<JsonList>&);
   void operator()(const std::string&);
   void operator()(double);
 
+  void checkKey();
   ConfigVisitor(std::map<int, Layer>& layerMap, std::map<double, double>& fitData, std::optional<double>& alpha);
   void setStream(std::ostream& stream);
 
@@ -21,4 +25,22 @@ private:
   std::map<double, double> _fitData;
   std::optional<double> _alpha;
   size_t _depth;
+
+  //private helper stuff
+  std::stack<ValueType> _helperStack;
+
+  bool is_digits_only(const std::string& str);
+  Material fillMaterial();
+  std::map<double, double> fillFitData();
+  void fillBaseField();
+
+  template <typename T>
+  T return_pop(std::stack<T>& s) { //returns the top element and pops stack IN THIS ORDER
+    if (s.empty()) throw std::runtime_error("Stack is empty");
+    T top = s.top();
+    s.pop();
+    return top;
+  }
 };
+
+//note to self: don't forget to differentiate between simulation and fitting!!!!
--- src/visitor.cpp
+++ src/visitor.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <map>
 #include <jsonsimplecpp/node.hpp>
 #include <jsonsimplecpp/parser.hpp>
+#include <complex>
 
+#include <forwardDecl.hpp>
+#include <matlayer.hpp>
 #include <visitor.hpp>
 
 
@@ -12,25 +16,75 @@ ConfigVisitor::ConfigVisitor(std::map<int, Layer>& layerMap, std::map<double, do
   _depth{0}
   {}
 
-void ConfigVisitor::operator()(const std::unique_ptr<JsonObject<ConfigVisitor>>& objectptr) {
-    for(auto it = objectptr->begin(); it != objectptr->end(); it++){
-        if(it->second == nullptr){ }
-    } 
-
-
-}
-
-void ConfigVisitor::operator()(const std::unique_ptr<JsonList<ConfigVisitor>>& listptr) {
-  *_out_stream << "[";
-  for (auto it = list->begin(); it != list->end(); ++it) {
-    std::visit(*this, (*it)->value);
-    auto next = it;
-    ++next;
-    if (next != list->end()) { *_out_stream << ", "; }
+void ConfigVisitor::operator()(const std::unique_ptr<JsonObject>& mapptr) {
+  for(auto it = mapptr->begin(); it != mapptr->end(); it++) {
+    _depth++;
+    if (it->second == nullptr) _depth > 1 ? _helperStack.push(it->first) : throw std::runtime_error("File format error: empty field!");
+    else if (std::holds_alternative<std::string>(it->second->value)) _helperStack.push(std::get<std::string>(it->second->value));
+    else if (std::holds_alternative<double>(it->second->value)) _helperStack.push(std::get<double>(it->second->value));
+    else {
+      _helperStack.push(it->first);
+      it->second->traverse();  // go deeper into map or list
+    }
   }
-  *_out_stream << "]";
 }
 
-void ConfigVisitor::operator()(const std::string& s) { *_out_stream << '"' << s << '"'; }
+void ConfigVisitor::operator()(const std::unique_ptr<JsonList>& listptr) {
+  _depth++;
+  for(auto it = listptr->begin(); it != listptr->end(); it++) {
+    if (*it == nullptr) throw std::runtime_error("File format error: empty field!");
+    else if (std::holds_alternative<std::string>((*it)->value)) _helperStack.push(std::get<std::string>((*it)->value));
+    else if (std::holds_alternative<double>((*it)->value)) _helperStack.push(std::get<double>((*it)->value));
+    else {(*it)->traverse();} // go deeper into map or list
 
-void ConfigVisitor::operator()(double num) { *_out_stream << num; }
+    if (_depth == 1) fillBaseField();
+  }
+  _depth--;
+}
+
+bool is_digits_only(const std::string& str) {
+  return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+void ConfigVisitor::fillBaseField() {
+
+  //fills layer
+  if (std::holds_alternative<std::string>(_helperStack.top()) && (
+  std::get<std::string>(_helperStack.top()).substr(0, 5) == (std::string)"Layer" || 
+  std::get<std::string>(_helperStack.top()).substr(0, 5) == (std::string)"layer")) {
+
+    bool layerEmitter;
+    double layerThickness;
+    Material layerMat;
+    size_t layerNum = std::stoi(std::get<std::string>(_helperStack.top()).substr(6,  std::get<std::string>(_helperStack.top()).length()));
+    
+    _helperStack.pop();
+    while(!_helperStack.empty()){
+      std::string subfield = std::get<std::string>(return_pop(_helperStack));
+        if (subfield == "thickness") layerThickness = std::get<double>(return_pop(_helperStack));
+        else if (subfield == "emitter") layerEmitter = static_cast<bool>(std::get<double>(return_pop(_helperStack)));
+        else if (subfield == "material") layerMat = fillMaterial();
+        else { throw std::runtime_error("wrongly formatted layer information!");}
+    }
+  }
+
+  //fills fitData
+  else if (std::holds_alternative<std::string>(_helperStack.top()) && (
+  std::get<std::string>(_helperStack.top()) == (std::string)"fitData" || 
+  std::get<std::string>(_helperStack.top()) == (std::string)"FitData")) {
+
+      _helperStack.pop();
+      _fitData = fillFitData();
+  }
+
+  //fill alpha
+  else if (std::holds_alternative<std::string>(_helperStack.top()) && (
+  std::get<std::string>(_helperStack.top()) == (std::string)"alpha" || 
+  std::get<std::string>(_helperStack.top()) == (std::string)"Alpha")) {
+
+      _helperStack.pop();
+      _alpha = std::get<double>(return_pop(_helperStack));
+  }
+  
+  else{throw std::runtime_error("misformatted JSON config file!");}
+}
