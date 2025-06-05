1dfd0fdd3e9cff8ee0709927a5bb14822fd563b8
moleculebits <lepton42@protonmail.ch>
2025-06-05 17:45:16 +0200

    sanitized the (previously ulgy) makefiles and added helper class (WIP)

---
+++ gui/include/threadhelper.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <QWidget>
+#include <QTimer>
+#include <QThreadPool>
+#include <QRunnable>
---
+++ gui/src/threadhelper.cpp
@@ -0,0 +1,13 @@
+#include <threadhelper.h>
+
+
+#include <basesolver.hpp>
+#include <fitting.hpp>
+#include <simulation.hpp>
+
+#include <QWidget>
+#include <QTimer>
+#include <QThreadPool>
+#include <QRunnable>
+
+
